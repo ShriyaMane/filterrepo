9fde2a14475ca0115a411f328088befaa5e8e197
ShriyaMane <shriyamane05@gmail.com>
2025-08-07 21:59:26 +0530

    Add WPF application entry point, main window, and project file for .NET 6.0 Windows Desktop application

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace FilterLifecycleTracker.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
